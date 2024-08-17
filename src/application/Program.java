07d48b90b3e139c22135af8ed576228415884982
Beterraba <ls6182315@gmail.com>
2024-08-17 16:23:06 -0300

    feat: create chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
     }
 }
