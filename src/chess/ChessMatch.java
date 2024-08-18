e72d32013a13b6be56dc948ee7dcb641fe57caec
Beterraba <ls6182315@gmail.com>
2024-08-18 15:02:36 -0300

    feat: improvement on board priting

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,7 +28,10 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('a',1,new Rook(board,Color.BLACK));
+        placeNewPiece('a',1,new Rook(board,Color.WHITE));
+        placeNewPiece('h',1,new Rook(board,Color.WHITE));
+        placeNewPiece('a',8,new Rook(board,Color.BLACK));
+        placeNewPiece('h',8,new Rook(board,Color.BLACK));
         placeNewPiece('e',7,new King(board,Color.WHITE));
     }
 }
